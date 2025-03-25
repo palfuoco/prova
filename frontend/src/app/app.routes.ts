2bd070e97929585a95a790438dd1a413a6aff52c
Luca Raffaele Caputo <cptlrf03l18d086j@studenti.unical.it>
2025-03-25 08:44:21 +0100

    Sistemato l'app root

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,4 +4,5 @@ import {HomeComponent} from './pages/home/home.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'ricettario', component: RicettarioComponent}
 ];
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { RicettarioComponent } from './app/pages/ricettario/ricettario.component';
 import { provideHttpClient } from '@angular/common/http';
-import {HomeComponent} from './app/pages/home/home.component';
+import {AppComponent} from './app/app.component';
 
-bootstrapApplication(HomeComponent, {
+bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [...(appConfig.providers || []), provideHttpClient()]
 }).catch((err) => console.error(err));
