540aa3e529ecefe36293ce4765301679087f15bc
LordBernie <lbernardini763@gmail.com>
2025-04-08 16:10:01 +0200

    Migliorata home e aggiunta pagina Login con routerLink

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {RicettarioComponent} from './pages/ricettario/ricettario.component';
 import {HomeComponent} from './pages/home/home.component';
+import {LoginComponent} from './pages/login/login.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'ricettario', component: RicettarioComponent}
+  {path: 'ricettario', component: RicettarioComponent},
+  {path: 'login', component: LoginComponent}
 ];
--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "../../components/header/header.component";
+import {FooterComponent} from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
