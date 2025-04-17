a373a65fc2bbb227758a32ad2549003a47441cfa
LordBernie <lbernardini763@gmail.com>
2025-04-17 16:24:20 +0200

    Fatto il login (frontend)

--- frontend/src/app/components/finestra-login/finestra-login.component.ts
+++ frontend/src/app/components/finestra-login/finestra-login.component.ts
@@ -1,10 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {NgClass} from '@angular/common';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-finestra-login',
   imports: [
-    NgClass
+    NgClass,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './finestra-login.component.html',
   styleUrl: './finestra-login.component.css'
